3433f3cc992f8bc7f5377109c5c51bc50c9eb35d
pawelek3113 <pawel.kamil.komendera@gmail.com>
2023-04-16 23:08:25 +0200

    Read files in modules directory

--- bot.py
+++ bot.py
@@ -9,11 +9,13 @@ load_dotenv()
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(intents=Intents.all())
+        for file in os.listdir("./modules"):
+            if file.endswith(".py"):
+                self.load_extension(f"modules.{file[:-3]}")
 
     async def on_ready(self):
-        print(f'Logged in as {self.user}')
+        print(f"Logged in as {self.user}")
 
 
 bot = Bot()
-
 bot.run(os.environ["TOKEN"])
